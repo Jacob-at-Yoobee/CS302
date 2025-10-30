7d94d62fd56f953aa74473adedc32a32c9bd44be
Jacob <270137506@yoobeestudent.ac.nz>
2025-10-31 09:43:42 +1300

    Update player.py

--- player.py
+++ player.py
@@ -6,3 +6,49 @@ themes = ["Cities",
           "Jobs",
           "Names",
           "Places"]
+
+slang_meanings = {"Rizz": "Charisma",
+                  "Nerd": "Know It All",
+                  "LOL": "Laughing Out Loud",
+                  "Cap": "Lies",
+                  "Bruh moment": "Stupid Mistake",
+                  "Deez Nuts": "Peanuts, Acorns, Almonds, Etc.",
+                  "Cringe": "Annoying",
+                  "Sus (adjective)": "Suspicious",
+                  "Sus (verb)": "Suspect",
+                  "Flip the Bird": "Give Someone The Middle Finger",
+                  "OG": "Original",
+                  "Drip": "High Fashion",
+                  "Zesty": "Fruity",
+                  "Chad": "Hunk",
+                  "Karen": "Entitled Person (often a woman)"}
+
+def slang_mode():
+    lives = 3
+    score = 0
+    print("Welcome to the Slang mode!")
+    print("In this mode, you must guess the meaning of a slang term.")
+    print("Good luck!")
+    while lives > 0:
+        print("Preparing word...")
+
+def gameplay():
+    modes = ["Slang",
+             "Chain",
+             "Opposites",
+             "Alpha-Thon",
+             "Rhyme Time",
+             "Translate",
+             "Contextual"]
+    selected = ""
+    while selected not in modes:
+        print("Welcome to Jacob's Word Game!")
+        print("There are seven game modes available.")
+        print("They are Slang, Chain, Alpha-Thon, Rhyme Time, Opposites, Translate and Contextual.")
+        selected = input("Which mode do you wish to play? ").title()
+    if selected == "Slang":
+        slang_mode()
+    else:
+        print("Invalid input!")
+
+gameplay()
